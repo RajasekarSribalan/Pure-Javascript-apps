37a3e79be132aebb73ec7626343f446586f92e3a
Rajasekar <emailtoraj90@gmail.com>
2017-09-28 20:30:00 +0530

    Integerated localstorage logic

--- Bookmarker/js/main.js
+++ Bookmarker/js/main.js
@@ -4,7 +4,30 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 
 function saveBookmark(e) {
-  console.log('Yipee!!!!');
 
+  // Get form values
+
+  var siteName = document.getElementById('siteName').value;
+  var siteURL = document.getElementById('siteURL').value;
+
+  var bookmark = {
+    name : siteName,
+    url : siteURL
+  };
+
+  //
+  if(localStorage.getItem('bookmarks') === null){
+    var bookmarks = [];
+    bookmarks.push(bookmark);
+
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+  }else {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);
+
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+  }
+
+  //Prevent form from submitting
   e.preventByDefault();
 }
