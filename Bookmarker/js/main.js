90ac0ae862f6e227613bdfe7fd182639fb4cfed9
Rajasekar <emailtoraj90@gmail.com>
2017-10-01 10:56:02 +0530

    Added additional content

--- Bookmarker/js/main.js
+++ Bookmarker/js/main.js
@@ -3,6 +3,31 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 
+//Fetch bookmarks
+function fetchBookmarks() {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  //Get output id
+
+
+  var boookmarkresults = document.getElementById('boookmarkresults');
+
+  boookmarkresults.innerHTML = '';
+
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    boookmarkresults.innerHTML += '<div class="well">' +
+      '<h3>' + name +
+      '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
+      '<a class="btn btn-danger" target="_blank" href="'+url+'">Delete</a>'
+      '</h3>' +
+      '</div>';
+
+  }
+}
+
 function saveBookmark(e) {
 
   // Get form values
@@ -11,21 +36,21 @@ function saveBookmark(e) {
   var siteURL = document.getElementById('siteURL').value;
 
   var bookmark = {
-    name : siteName,
-    url : siteURL
+    name: siteName,
+    url: siteURL
   };
 
   //
-  if(localStorage.getItem('bookmarks') === null){
+  if (localStorage.getItem('bookmarks') === null) {
     var bookmarks = [];
     bookmarks.push(bookmark);
 
-    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
-  }else {
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     bookmarks.push(bookmark);
 
-    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
 
   //Prevent form from submitting
